ef9c0677d7700bb058969d82a1e22b526b22b335
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-10-18 16:53:00 +0200

    Calculate people pairs number amd people pairs&days number

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,7 +1,8 @@
 import math
 from decimal import Decimal
 from itertools import combinations
-
+from random import *
+from math import *
 
 
 def main():
@@ -13,15 +14,58 @@ def main():
     prob1 = prob*prob*(1/numHotels)
     prob2 = prob1**2
 
-    cEstimated=(n**2/2)*(numDays**2/2)*prob2
+    cEstimated=int((n**2/2)*(numDays**2/2)*prob2)
 
-    print(prob1,prob2,cEstimated)
+    print("Spodziewane wartości ",cEstimated)
+
+    dictMain = {}
+    #make {dict[day]:{numhotel:{list of people}}}
+    for i in range(1,numDays+1):
+        dictMain[i]={}
+        for j in range(1,n+1):
+            isGo=choices([1,0],[0.1,0.9])
+            if (isGo[0]==1):
+                numHotel=randint(1,numHotels)
+                if(numHotel not in dictMain[i]):
+                    dictMain[i][numHotel]=[]
+                    dictMain[i][numHotel].append(j)
+                else:
+                    dictMain[i][numHotel].append(j)
 
 
+    #make combination pairs of people in dict[day][numHot]
+    dictPom={}
+    for day in dictMain:
+        dictPom[day]={}
+        for numHot in dictMain[day]:
+            pairsComb=combinations(dictMain[day][numHot],2)
+            dictPom[day][numHot]=list(pairsComb)
 
-    comb=combinations(range(n),2)
-    print(list(comb))
 
+    #make pairs counter dict
+    dictPairCount={}
+    for day in dictPom:
+        for numHot in dictPom[day]:
+            for para in dictPom[day][numHot]:
+                if (para not in dictPairCount):
+                    dictPairCount[para]=1
+                else:
+                    dictPairCount[para]+=1
 
+    numberOfPairsPeople=0
+    numberOfPairsPeopleDays=0
+
+    for x in dictPairCount:
+        if dictPairCount[x]>1:
+            numberOfPairsPeople+=1
+            #numberOfPairsPeopleDays+=dictPairCount[x]
+            #number of combinations pair's days
+            numberOfPairsPeopleDays += (factorial(dictPairCount[x])/(factorial(2)*factorial(dictPairCount[x]-2)))
+
+    print("Liczba podejrzanych par " ,numberOfPairsPeople)
+    print("Liczba podejrzanych par ludzi i dni" , int(numberOfPairsPeopleDays))
+    #TODO
+    #liczba ogolnie podejrzanych osob
+    #histogram postaci ilosc spotkan - os x ; liczba par o takiej ilosci - os y
 if __name__=="__main__":
     main()
