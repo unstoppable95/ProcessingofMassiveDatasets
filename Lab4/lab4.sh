1bb26876d0e280c5c77a270fd12e837e6b0e1f36
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-11-08 22:57:12 +0100

    Finish all tasks and add full results to Readme

--- Lab4/lab4.sh
+++ Lab4/lab4.sh
@@ -23,11 +23,6 @@ awk -F ";" '{OFS=";"; x=strftime("%Y;%m;%d", $3); print NR ";" x}' NEW_triplets_
 rm NEW_unique_tracks.txt
 rm NEW_triplets_sample.txt
 
-#concat files
-#awk -F ";" 'FNR==NR{A[$2]=$2;next} A[$2]{print $1 t A[$2]}' t=";" tracks.txt samples.txt 
-#delete repeates
-#awk -F ";" '!seen[$1,$2]++' 
-
 #task 1
 awk -F ";" 'FNR==NR{A[$2]=$3t$4t$2;next} A[$2]{print A[$2]}' t=";" tracks.txt samples.txt | awk -F";" '{col[$1t$2t$3]++} END {for (i in col) print i ";" col[i]}' t=";" | sort -t";" -n -k4 -r | head -n 10 | cut -d ";" -f1,2,4 |tr ";" " "
 
@@ -41,3 +36,13 @@ awk -F ";" 'FNR==NR{A[$2]=$3;next} A[$2]{print A[$2]t$2}' t=";" tracks.txt sampl
 join -1 3 -2 1 samples.txt dates.txt -t";" -o'2.3'| sort  | uniq -c  | awk '{print $2,$1}'
 
 #task 5
+#select song_id of 3 Queen's most popular songs
+awk -F ";" 'FNR==NR{A[$2]=$3;next} A[$2]{print $2 t A[$2]}' t=";" tracks.txt samples.txt | awk -F ";" '{if ($2 == "Queen") print $0;}' | awk -F ";" '{col[$1]++} END {for (i in col) print i ";"col[i]}' | sort -n -t";" -k2 -r | head -n 3 | cut -d ";" -f1 | awk '{print $1";"}'> Queens_3_songs.txt
+
+#select 10 users,which listen all of 3 most popular Queen's songs
+awk -F ";" 'NR==FNR{ a[$1]=$1; next }$2 in a{ print $1t$2 }' t=";" Queens_3_songs.txt samples.txt | sort -u -t";" | awk -F ";" '{col[$1]++} END {for (i in col) print i ";"col[i]}' | grep ';3' |cut -d ";" -f1 | sort | head -n 10
+
+rm Queens_3_songs.txt
+
+#if you want only console result and save some space on disk
+#rm dates.txt tracks.txt samples.txt
