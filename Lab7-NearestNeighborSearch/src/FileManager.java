860c0e26a124907d6276d9f6121a0e72d45cda4a
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2019-01-06 20:46:52 +0100

    Complete save to file

--- Lab7-NearestNeighborSearch/src/FileManager.java
+++ Lab7-NearestNeighborSearch/src/FileManager.java
@@ -1,42 +1,61 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class FileManager {
-    private String pathToFile;
+    private String pathToInputFile;
 
     public FileManager(String pathToFile) {
-        this.pathToFile = pathToFile;
+        this.pathToInputFile = pathToFile;
     }
 
-    public Map<Integer,ArrayList<Integer>>  makeUsersStructure() {
-        Map<Integer,ArrayList<Integer>> userUniqueSongs = new HashMap<>();
+    public Map<Integer, ArrayList<Integer>> makeUsersStructure() {
+        Map<Integer, ArrayList<Integer>> userUniqueSongs = new HashMap<>();
         try {
-            BufferedReader  reader = new BufferedReader(new FileReader(this.pathToFile));
+            BufferedReader reader = new BufferedReader(new FileReader(this.pathToInputFile));
             String line;
             reader.readLine();
             while ((line = reader.readLine()) != null) {
-
-                int [] lineContent = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
-
-                if (userUniqueSongs.get(lineContent[0]) != null){
-                    if (!userUniqueSongs.get(lineContent[0]).contains(lineContent[1])){
+                int[] lineContent = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+                if (userUniqueSongs.get(lineContent[0]) != null) {
+                    if (!userUniqueSongs.get(lineContent[0]).contains(lineContent[1])) {
                         userUniqueSongs.get(lineContent[0]).add(lineContent[1]);
                     }
-                }
-                else{
-                    ArrayList<Integer> tmp=new ArrayList();
+                } else {
+                    ArrayList<Integer> tmp = new ArrayList();
                     tmp.add(lineContent[1]);
-                    userUniqueSongs.put((lineContent[0]) , tmp);
+                    userUniqueSongs.put((lineContent[0]), tmp);
                 }
 
             }
             reader.close();
-        }catch(IOException e ){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return  userUniqueSongs;
+        return userUniqueSongs;
+    }
+
+    public void saveResultsToFile(int userId, ArrayList<ArrayList<Object>> userList) {
+
+        try {
+            FileWriter file = new FileWriter("Results.txt", true);
+            BufferedWriter writer1 = new BufferedWriter(file);
+            PrintWriter writer = new PrintWriter(writer1);
+            writer.write("User = " + userId);
+            writer.println();
+            int numberOfIterations = (userList.size() < 100) ? userList.size() : 100;
+
+            for (int i = 0; i < numberOfIterations; i++) {
+                writer.write(String.format("%7d %.5f", userList.get(i).get(0), userList.get(i).get(1)));
+                writer.println();
+            }
+
+            writer.println();
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd przy zapisie pliku ");
+        }
+
     }
 
 }
--- Lab7-NearestNeighborSearch/src/Main.java
+++ Lab7-NearestNeighborSearch/src/Main.java
@@ -1,20 +1,16 @@
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
 
-
     public static void main(String[] args) {
-
-        FileManager manager= new FileManager("./data/facts-nns.csv");
-
-
+        long startTime = System.currentTimeMillis();
+        FileManager manager = new FileManager("./data/facts-nns.csv");
         Map<Integer, ArrayList<Integer>> userUniqueSongs = manager.makeUsersStructure();
 
-        for (Map.Entry<Integer,ArrayList<Integer>> user: userUniqueSongs.entrySet()) {
-            if (user.getKey() <= 1) {
-                ArrayList<ArrayList<Object>> userList= new ArrayList<>();
+        for (Map.Entry<Integer, ArrayList<Integer>> user : userUniqueSongs.entrySet()) {
+            if (user.getKey() <= 100) {
+                ArrayList<ArrayList<Object>> userList = new ArrayList<>();
 
                 for (Map.Entry<Integer, ArrayList<Integer>> userTmp : userUniqueSongs.entrySet()) {
                     int intersectVal = 0;
@@ -40,7 +36,7 @@ public class Main {
                     int sumJacckard = userTmp.getValue().size() + user.getValue().size() - intersectVal;
 
                     if (sumJacckard != 0 && intersectVal != 0) {
-                        double jacckardRat = (double)intersectVal / (double)sumJacckard;
+                        double jacckardRat = (double) intersectVal / (double) sumJacckard;
                         ArrayList<Object> listInner = new ArrayList();
                         listInner.add(userTmp.getKey());
                         listInner.add(jacckardRat);
@@ -48,14 +44,23 @@ public class Main {
                     }
 
                 }
-                InstanceCompare sort = new InstanceCompare();
-                userList.sort(sort);
-                for(int i =0 ; i<=100; i++){
-                    System.out.println(userList.get(i).get(0) +" " + userList.get(i).get(1));
-                }
+
+
+                Collections.sort(userList, new Comparator<ArrayList<Object>>() {
+                    @Override
+                    public int compare(ArrayList<Object> a, ArrayList<Object> b) {
+                        if ((double) a.get(1) < (double) b.get(1)) return 1;
+                        if ((double) a.get(1) > (double) b.get(1)) return -1;
+                        else return 0;
+                    }
+                });
+
+                manager.saveResultsToFile(user.getKey(), userList);
+
             }
         }
-
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Total time: " + estimatedTime/1000 + " seconds ");
     }
 
 
