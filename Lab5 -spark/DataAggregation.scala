275c488834b4517cb61eecf2a15173cdddd5530b
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2019-01-06 16:50:32 +0100

    Add cache

---
+++ Lab5 -spark/DataAggregation.scala
@@ -0,0 +1,29 @@
+import  scala.util.Random
+
+val  groups = Map(0-> (0 ,1) , 1-> (1 ,1) , 2-> (2 ,2) ,  3->(3,3) ,4->(4,3) ,  5->(5,2) ,  6->(6,1))
+val n = 1000000
+val  random = Random
+
+val  data = sc.parallelize(for (i<-1 to n) yield { val g=random.nextInt(7); val (mu,sigma) = groups(g); (g, mu+sigma*random.nextGaussian())})
+data.cache
+
+//for groups
+val resultsForGroupsPom= data.map(t=>(t._1,(1,t._2,t._2*t._2))).reduceByKey((a,b)=>(a._1+b._1,a._2+b._2,a._3+b._3))
+resultsForGroupsPom.cache
+
+
+// group , (number of examples, average, variance)
+val resultsForGroups=resultsForGroupsPom.map(p=>(p._1,(p._2._1,(p._2._2/p._2._1),(p._2._3/p._2._1)-(p._2._2/p._2._1)*(p._2._2/p._2._1))))
+
+resultsForGroups.collect().foreach(println)
+
+//for all
+//(0,(number of examples, sum, sum*sum))
+val dataAll =resultsForGroupsPom.reduce((a,b) =>(0,(a._2._1+b._2._1, a._2._2+b._2._2, a._2._3+b._2._3)))
+
+
+//(number of examples, average, variance)
+val resultsAll = (dataAll._2._1, (dataAll._2._2/dataAll._2._1), (dataAll._2._3/dataAll._2._1)-(dataAll._2._2/dataAll._2._1)*(dataAll._2._2/dataAll._2._1))
+
+
+println(resultsAll)
---
+++ Lab5 -spark/SparkMsdc.scala
@@ -0,0 +1,61 @@
+//Read data
+val songs = spark.read.option("delimiter",";").csv("tracks.txt").
+toDF("track_id","song_id", "artist","song")
+
+val facts = spark.read.option("delimiter",";").csv("samples.txt").
+toDF( "user_id","song_id","date_id")
+
+val dates = spark.read.option("delimiter",";").csv("dates.txt").
+toDF( "date_id","day","month", "day")
+
+//query 1 -> The most popular songs
+facts.groupBy("song_id").
+count.join(songs,facts("song_id")===songs("song_id")).
+select("song","artist","count").
+orderBy(desc("count")).
+show(10,false)
+
+//query 2 -> Top 10 users who listened the biggest number of unique songs 
+facts.select("user_id","song_id").
+distinct().
+groupBy("user_id").
+count().
+select("user_id","count").
+orderBy(desc("count")).
+show(10,false)
+
+//query 3 -> The most popular artist 
+facts.select("song_id").
+join(songs, "song_id").
+groupBy("artist").
+count().
+select("artist","count").
+orderBy(desc("count")).
+show(1)
+
+//query 4 -> The most popular songs for each month
+facts.join(dates, "date_id").
+groupBy("month").
+count().
+select("month","count").
+orderBy("month").
+show(12)
+
+//query 5 -> 10 users who listened all 3 most popular songs performed by Queen 
+val Queens3Popular = songs.select("song_id","artist").
+filter($"artist".isin("Queen")).
+join(facts, "song_id").
+groupBy("song_id").
+count().
+orderBy(desc("count")).
+select("song_id").limit(3)
+
+facts.select("user_id","song_id").
+join(Queens3Popular,"song_id").
+distinct().
+groupBy("user_id").
+count().
+filter($"count">=3).
+orderBy("user_id").
+select("user_id").
+show(10,false)
